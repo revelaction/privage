32400084837b38ea68213cf53c9b227d73b2b677
revelaction <96388231+revelaction@users.noreply.github.com>
2022-02-20 12:47:08 +0100

    dump write

---
+++ cmd/privage/write.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"crypto/sha256"
+	"fmt"
+	"io"
+	"os"
+
+	"filippo.io/age"
+
+	"github.com/revelaction/privage/header"
+	id "github.com/revelaction/privage/identity"
+	"github.com/revelaction/privage/setup"
+)
+
+// encryptSave encrypts the Header h and the content separately and
+// concatenates both encrypted payloads.
+//
+// It saves the concatenated encrypted payloads on an age file. The name of the
+// file is a hash of the header (label and category) and the public age key.
+func encryptSave(h *header.Header, suffix string, content io.Reader, s *setup.Setup) error {
+
+	// age io.Writer
+	buf := new(bytes.Buffer)
+	ageWr, err := age.Encrypt(buf, s.Id.Id.Recipient())
+	if err != nil {
+		fatal(err)
+	}
+
+	_, err = ageWr.Write(h.Pad())
+	if err != nil {
+		return err
+	}
+
+	if err = ageWr.Close(); err != nil {
+		fatal(err)
+	}
+
+	headerPadded, _ := header.PadEncrypted(buf.Bytes()) // []byte
+
+	filePath := s.Repository + "/" + fileName(h, s.Id, suffix)
+
+	f, err := os.OpenFile(filePath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0600)
+	if err != nil {
+		return err
+	}
+
+	defer func() {
+		if err := f.Close(); err != nil {
+			return
+		}
+	}()
+	_, err = f.Write(headerPadded)
+	if err != nil {
+		return err
+	}
+
+	bufContent := bufio.NewReader(content)
+
+	bufFile := bufio.NewWriter(f)
+
+	ageContentWr, err := age.Encrypt(bufFile, s.Id.Id.Recipient())
+	if err != nil {
+		fatal(err)
+	}
+
+	if _, err := io.Copy(ageContentWr, bufContent); err != nil {
+		fatal(err)
+	}
+
+	if err = ageContentWr.Close(); err != nil {
+		fatal(err)
+	}
+
+	bufFile.Flush()
+
+	return nil
+}
+
+// fileName generates the file name of a privage encrypted file.
+// The hash is a function of the header and the age public key.
+func fileName(h *header.Header, identity id.Identity, suffix string) string {
+	hash := append(h.Pad(), identity.Id.Recipient().String()...)
+	hashStr := fmt.Sprintf("%x", sha256.Sum256(hash))
+	return hashStr + suffix + AgeExtension
+}
