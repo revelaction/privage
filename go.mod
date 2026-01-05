85584d77b47d837b9f1895232468b560c5c61cb8
revelaction <96388231+revelaction@users.noreply.github.com>
2026-01-05 16:17:23 +0100

    test: add integration tests using testscript
    - Added 'tests/' directory with 'testscript' harness.
    - Added 4 test scenarios: basic_flow, lifecycle, crypto, errors.
    - Verified all tests pass.

--- go.mod
+++ go.mod
@@ -7,10 +7,12 @@ require (
 	github.com/atotto/clipboard v0.1.4
 	github.com/go-piv/piv-go/v2 v2.4.0
 	github.com/pelletier/go-toml/v2 v2.2.4
+	github.com/rogpeppe/go-internal v1.14.1
 )
 
 require (
 	filippo.io/hpke v0.4.0 // indirect
 	golang.org/x/crypto v0.45.0 // indirect
 	golang.org/x/sys v0.38.0 // indirect
+	golang.org/x/tools v0.39.0 // indirect
 )
---
+++ tests/main_test.go
@@ -0,0 +1,54 @@
+package tests
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"runtime"
+	"testing"
+
+	"github.com/rogpeppe/go-internal/testscript"
+)
+
+var binDir string
+
+func TestMain(m *testing.M) {
+	// 1. Setup: Build binary
+	var err error
+	binDir, err = os.MkdirTemp("", "privage-test-bin")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to create temp dir: %v\n", err)
+		os.Exit(1)
+	}
+	defer os.RemoveAll(binDir)
+
+	binPath := filepath.Join(binDir, "privage")
+	if runtime.GOOS == "windows" {
+		binPath += ".exe"
+	}
+
+	// Build from ../cmd/privage relative to this file
+	// We assume "go" is in the path
+	cmd := exec.Command("go", "build", "-o", binPath, "../cmd/privage")
+	if out, err := cmd.CombinedOutput(); err != nil {
+		fmt.Fprintf(os.Stderr, "failed to build privage: %v\n%s\n", err, out)
+		os.Exit(1)
+	}
+
+	// 2. Run
+	os.Exit(testscript.RunMain(m, map[string]func() int{}))
+}
+
+func TestScript(t *testing.T) {
+	testscript.Run(t, testscript.Params{
+		Dir: "testdata",
+		Setup: func(env *testscript.Env) error {
+			// Prepend binDir to PATH so "privage" command is found
+			env.Vars = append(env.Vars, fmt.Sprintf("PATH=%s%c%s", binDir, filepath.ListSeparator, os.Getenv("PATH")))
+			// Set HOME to the test work directory to allow config file creation
+			env.Vars = append(env.Vars, fmt.Sprintf("HOME=%s", env.WorkDir))
+			return nil
+		},
+	})
+}
